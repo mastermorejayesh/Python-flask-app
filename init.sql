37b58796b797f30c40fbb5e67dcf5fcfb2d02dc8
Jayesh More <147997145+mastermorejayesh@users.noreply.github.com>
2025-02-09 19:34:46 +0530

    Create init.sql
    adding init.sql file

---
+++ init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE flask_python;
+USE flask_python;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE usersheader (header VARCHAR(100));
+
+INSERT INTO users (name, email) VALUES
+('Babita', 'Babita@example.com'),
+('jethalal', 'jethalal@example.com');
+INSERT INTO usersheader(header) VALUES ('JAYESH-MORE');
